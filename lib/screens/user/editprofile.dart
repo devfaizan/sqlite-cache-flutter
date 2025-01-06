f66ae8aaa3fb37b48f1baf85b3fcf7d85163a090
devfaizan <mfaizan.zahir@gmail.com>
2025-01-07 02:07:54 +0500

    added form to edit profile screen

--- lib/screens/user/editprofile.dart
+++ lib/screens/user/editprofile.dart
@@ -4,10 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sqlsqlsql/models/users.dart';
 import 'package:sqlsqlsql/provider/userformprovider.dart';
+import 'package:sqlsqlsql/utils/validation.dart';
 import 'package:sqlsqlsql/widgets/drawer/drawer.dart';
+import 'package:sqlsqlsql/widgets/inputwidget.dart';
+import 'package:sqlsqlsql/widgets/primarybutton.dart';
 
 class EditProfileScreen extends StatefulWidget {
-  final User user;
+  final User? user;
   const EditProfileScreen({
     super.key,
     required this.user,
@@ -18,49 +21,119 @@ class EditProfileScreen extends StatefulWidget {
 }
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
+  TextEditingController nameController = TextEditingController();
+  final _key = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    nameController = TextEditingController(text: widget.user?.name ?? '');
+  }
+
   @override
   Widget build(BuildContext context) {
     final userFormProvider = Provider.of<UserFormProvider>(context);
+    final heightContext = MediaQuery.of(context).size.height;
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.user.name),
-          centerTitle: true,
-        ),
-        drawer: AppDrawer(),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 100,
-                  backgroundImage: FileImage(
-                    File(widget.user.image),
-                  ),
+      appBar: AppBar(
+        title: Text(widget.user!.name),
+        centerTitle: true,
+      ),
+      drawer: AppDrawer(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: FileImage(
+                  File(widget.user!.image),
                 ),
-              ],
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  userFormProvider.pickImage();
+                },
+                child: Text("Change Image"),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? const Color.fromARGB(255, 244, 244, 244)
+                      : const Color.fromARGB(255, 49, 47, 47),
+                ),
+                child: ListView(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                  ),
+                  children: <Widget>[
+                    SizedBox(
+                      height: heightContext < 830
+                          ? heightContext / 30
+                          : heightContext / 10,
+                    ),
+                    Form(
+                      key: _key,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InputWidget(
+                            controller: nameController,
+                            hint: "User Name",
+                            label: "Enter Your Name",
+                            preicon: Icons.person,
+                            iconsize: 25.0,
+                            validation: validateText,
+                            action: TextInputAction.done,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    userFormProvider.pickImage();
-                  },
-                  child: Text("Change Image"),
-                ),
-              ],
+          ),
+          SizedBox(
+            height: heightContext < 830 ? 10 : 20,
+          ),
+          Column(
+            children: [
+              PrimaryButton(
+                text: "Update",
+                onPressed: () async {},
+                borderRadius: BorderRadius.circular(5),
+                textsize: 20,
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: heightContext < 830 ? 10 : 30,
             ),
-            Row(),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
