e15c5ee4db5c7082d765d6590f9419d4da19beb8
devfaizan <mfaizan.zahir@gmail.com>
2025-01-11 23:32:35 +0500

    initial singlepet view

--- lib/screens/pet/singlepetview.dart
+++ lib/screens/pet/singlepetview.dart
@@ -1,5 +1,11 @@
+import "dart:io";
+import "dart:ui";
+
 import "package:flutter/material.dart";
 import "package:sqlsqlsql/models/cats.dart";
+import "package:sqlsqlsql/screens/pet/updatepets.dart";
+import "package:sqlsqlsql/utils/colors.dart";
+import "package:sqlsqlsql/utils/outputtext.dart";
 
 class SinglePetScreen extends StatefulWidget {
   final Pet pet;
@@ -15,13 +21,128 @@ class SinglePetScreen extends StatefulWidget {
 class _SinglePetScreenState extends State<SinglePetScreen> {
   @override
   Widget build(BuildContext context) {
+    final heightContext = MediaQuery.of(context).size.height;
+    final widthContext = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.pet.name),
         centerTitle: true,
       ),
       body: Center(
-        child: Text(widget.pet.type),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            children: [
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: widthContext,
+                        height: heightContext / 1.6,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                            image: FileImage(
+                              File(widget.pet.image),
+                            ),
+                            // opacity: .8,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    bottom: -30,
+                    left: 35,
+                    child: Center(
+                      child: ClipRect(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(
+                            sigmaX: 2.0,
+                            sigmaY: 2.0,
+                          ),
+                          child: Container(
+                            width: widthContext / 1.2,
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 20,
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Colors.grey.shade200.withOpacity(0.3),
+                              gradient: LinearGradient(
+                                colors: [
+                                  colorLightPurple,
+                                  colorGreenAccent,
+                                ],
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                NormalText(
+                                  text: widget.pet.type,
+                                ),
+                                SubheadingText(
+                                  text: widget.pet.name,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => UpdateScreen(pet: widget.pet),
+                        ),
+                      );
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.edit,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 5,
+                          ),
+                        ),
+                        Text(
+                          "Edit",
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 5,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
